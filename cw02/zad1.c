25ad8b521bd91ebaf29d6f6680e59d59e9f1f3b6
enkelian <aniano13@gmail.com>
2020-03-12 18:02:07 +0100

    Morbid death

--- cw02/zad1.c
+++ cw02/zad1.c
@@ -23,8 +23,8 @@ void delete_strings(char** strings, int line_count){
 	free(strings);
 }
 char** create_random(int line_count, int line_len){
-	int n = 94;
-	int start = 32;
+	int n = 26;
+	int start = 97;
 	char chars[n];
 	for(int i = 0; i<n; i++){
 		chars[i] = (char) (start + i);
@@ -107,6 +107,33 @@ void swap_lines_lib(FILE* file, int i, int j, int line_len){
 	free(line2);
 }
 
+int partition_lib(FILE* file, int l, int h, int line_len){
+	char* pivot = calloc(line_len+1, sizeof(char));
+	fseek(file, (line_len+1)*h, 0);
+	fread(pivot, sizeof(char), line_len+1, file);
+	int i = l - 1;
+	char* curr = calloc(line_len+1, sizeof(char));
+	for(int j = l; j<h; j++){
+		fseek(file, (line_len+1)*j, 0);
+		fread(curr, sizeof(char), line_len+1, file);
+		if(strcmp(curr,pivot)<0){
+			swap_lines_lib(file,++i,j,line_len);
+		}
+	}
+	free(curr);
+	free(pivot);
+	swap_lines_lib(file, ++i, h, line_len);
+	return i;
+}
+
+void quick_sort_lib(FILE* file, int l, int h, int line_len){
+	if(l<h){
+		int p = partition_lib(file, l, h, line_len);
+		quick_sort_lib(file, l, p-1, line_len);
+		quick_sort_lib(file, p+1, h, line_len);
+	}
+}
+
 void swap_lines_sys(int fd, int i, int j, int line_len){
 	char* line1 = calloc(line_len+1, sizeof(char));
 	char* line2 = calloc(line_len+1, sizeof(char));
@@ -126,14 +153,55 @@ void swap_lines_sys(int fd, int i, int j, int line_len){
 	free(line2);
 }
 
+int partition_sys(int fd, int l, int h, int line_len){
+	char* pivot = calloc(line_len+1, sizeof(char));
+	lseek(fd, (line_len+1)*h, 0);
+	read(fd, pivot, line_len+1);
+	int i = l -1;
+	char* curr = calloc(line_len+1, sizeof(char));
+	for(int j = l; j<h; j++){
+		lseek(fd, (line_len+1)*j, 0);
+		read(fd, curr, line_len+1);
+		if(strcmp(curr, pivot)<0){
+			swap_lines_sys(fd, ++i, j, line_len);
+		}
+	}
+	free(curr);
+	free(pivot);
+	swap_lines_sys(fd, ++i, h, line_len);
+	return i;
+}
+
+void quick_sort_sys(int fd, int l, int h, int line_len){
+	if(l<h){
+		int p = partition_sys(fd, l, h, line_len);
+		quick_sort_sys(fd, l, p-1, line_len);
+		quick_sort_sys(fd, p+1, h, line_len);
+	}
+}
+
+void sort(char* file_name, int line_count, int line_len, int use_f){
+	if(use_f == 1){
+		FILE* file = fopen(file_name, "r+");
+		quick_sort_lib(file, 0, line_count - 1, line_len);
+		fclose(file);
+	}
+	else{
+		int fd = open(file_name, O_RDWR);
+		quick_sort_sys(fd, 0, line_count - 1, line_len);
+		close(fd);
+	}
+}
+
 int main(int argc, char** argv){
-//	generate("dupa.txt", 5, 5, 0);
+//	generate("dupa.txt", 5, 5, 1);
 //	copy("dupa.txt","chuj.txt",5,5,0);
 //	FILE* file = fopen("dupa.txt", "r+");
 //	swap_lines_lib(file,1,2,5);
 //	fclose(file);
-	int fd = open("dupa.txt", O_RDWR);
-	swap_lines_sys(fd, 1, 2, 5);
-	close(fd);
+//	int fd = open("dupa.txt", O_RDWR);
+//	swap_lines_sys(fd, 1, 2, 5);
+//	close(fd);
+	sort("dupa.txt", 5, 5, 0);
 }
 
