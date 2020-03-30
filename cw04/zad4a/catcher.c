df08f69b532dbef5a26030ce9988982afac85acf
enkelian <aniano13@gmail.com>
2020-03-30 18:09:39 +0200

    Drenched in your scorn

--- cw04/zad4a/catcher.c
+++ cw04/zad4a/catcher.c
@@ -8,35 +8,75 @@
 int counter = 0;
 int catching = 1;
 pid_t sender_pid;
+int sig_1 = SIGUSR1;
+int sig_2 = SIGUSR2;
 
 
-void sigusr1_action(int sig, siginfo_t* info, void* ucontext){
-	counter++;
-}
-
-void sigusr2_action(int sig, siginfo_t* info, void* ucontext){
-	catching = 0;
-	sender_pid = info->si_pid;
+void sigusr_action(int sig, siginfo_t* info, void* ucontext){
+	if(sig == sig_1){
+		counter++;
+	}
+	else if(sig == sig_2){
+		catching = 0;
+		sender_pid = info->si_pid;
+	}
 }
 
 int main(int argc, char** argv){
 
+	if(argc < 2){
+		printf("Invalid arguments. Expected: kill/sigqueue/sigrt\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	if(strcmp(argv[1], "sigrt") == 0){
+		sig_1 = SIGRTMIN;
+		sig_2 = SIGRTMIN+1;
+	}
 
 	printf("Catcher PID %d\n",getpid());
 
 	struct sigaction action;
-	action.sa_flags = SA_SIGINFO;
-	sigfillset(&action.sa_mask);
-	sigdelset(&action.sa_mask, SIGUSR1);
-	sigdelset(&action.sa_mask, SIGUSR2);
 
-	sigaction()
+	action.sa_flags = SA_SIGINFO;
+
+	sigfillset(&action.sa_mask);
+	sigdelset(&action.sa_mask, sig_1);
+	sigdelset(&action.sa_mask, sig_2);
+
+	action.sa_sigaction = sigusr_action;
+
+	sigaction(sig_1, &action, NULL);
+	sigaction(sig_2, &action, NULL);
+
+
+
 	while(catching){
-		usleep(100);
+		usleep(1);
+	}
+
+	if(strcmp(argv[1], "sigqueue") == 0){
+		union sigval sigval;
+		for(int i = 0; i<counter; i++){
+			sigval.sival_int = i;
+			sigqueue(sender_pid, sig_1, sigval);
+		}
+		sigval.sival_int = counter;
+		sigqueue(sender_pid, sig_2, sigval);
+	}
+	else{
+		for(int i = 0; i<counter; i++){
+			kill(sender_pid, sig_1);
+		}
+		kill(sender_pid, sig_2);
 	}
 
 
+	printf("Received %d signals in catcher\n",counter);
 
+
+	exit(EXIT_SUCCESS);
 
 
 }
